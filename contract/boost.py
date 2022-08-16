ab88a6e19c3ecec24f0303d4d3dfdaea846b0b9e
mac <yeqingxiang0828@163.com>
2022-08-16 16:24:11 +0800

    封装了方法

--- contract/boost.py
+++ contract/boost.py
@@ -3,31 +3,57 @@ from app import *
 ken3, swap_router, swap_mining, boost, gauge = init()
 print(boost.address)
 
-# pool_length 池子数量
-pool_length = boost.functions.poolLength().call()
-print("池子数量:", pool_length)
+def get_pool_length():
+    # pool_length 池子数量
+    pool_length = boost.functions.poolLength().call()
+    return pool_length
+pool_length = get_pool_length()
 
-# pool_info 池子信息
-print("池子lp地址", "池子权重", "上次池子更新的块 Farming APR")
-for i in range(pool_length):
-    poolInfo = boost.functions.poolInfo(i).call()
+def get_pool_Info():
+    # pool_info 池子信息
+    print("池子lp地址", "池子权重", "上次池子更新的块 Farming APR")
+    for i in range(pool_length):
+        poolInfo = boost.functions.poolInfo(i).call()
 
-    # totalAllocPoint：查询所有池子的总权重
+        # totalAllocPoint：查询所有池子的总权重
+        totalAllocPoint = boost.functions.totalAllocPoint().call()
+
+        # 📚Farming APR（仅克莱因有）
+        # = (KEN单块产量*28800 )*（该池子权重 / 所有池子权重 ）* KEN价格 / 池子TVL*365/3.33*100%
+        farming_apr = (0.4 * 28800) * (poolInfo[1] / totalAllocPoint) * 0.57 / 27.97 * 365 / 3.33
+        print("池子:{}".format(i + 1), poolInfo, '{:.10%}'.format(farming_apr))
+    return poolInfo
+
+def get_votes(tokenID,address):
+    # votes：查询加速之后个人池子权重 参数0 uint256:输入tokenID 参数1 address:输入池子lp地址，不是加密池子可使用池子地址
+    votes = boost.functions.votes(tokenID, address).call()
+    return votes
+
+def get_weights(address):
+    # usedWeights：查询加速之后个人池子总权重 参数 address:输入池子lp地址
+    weights = boost.functions.weights(address).call()
+    return weights
+
+def get_tokenPerBlock():
+    # tokenPerBlock 每个块产生的奖励数量
+    tokenPerBlock = boost.functions.tokenPerBlock().call()
+    return tokenPerBlock
+
+def get_totalAllocPoint():
     totalAllocPoint = boost.functions.totalAllocPoint().call()
+    return totalAllocPoint
 
-    # 📚Farming APR（仅克莱因有）
-    # = (KEN单块产量*28800 )*（该池子权重 / 所有池子权重 ）* KEN价格 / 池子TVL*365/3.33*100%
-    farming_apr = (0.4 * 28800) * (poolInfo[1] / totalAllocPoint) * 0.57 / 27.97 * 365 / 3.33
-    print("池子:{}".format(i + 1), poolInfo, '{:.10%}'.format(farming_apr))
-print("")
-
-print('查询所有池子的总权重', totalAllocPoint)
-
-# votes：查询加速之后个人池子权重 参数0 uint256:输入tokenID 参数1 address:输入池子lp地址，不是加密池子可使用池子地址
-votes = boost.functions.votes(50, "0xB92524021c43f663F78dbD56Ed5007E110F94998").call()
-print("查询加速之后个人池子权重", votes)
-
-# usedWeights：查询加速之后个人池子总权重 参数 address:输入池子lp地址
-weights = boost.functions.weights("0xB92524021c43f663F78dbD56Ed5007E110F94998").call()
-print("查询加速之后个人池子总权重", weights)
+if __name__ == '__main__':
+    pool_length = get_pool_length()
+    print("池子数量",pool_length)
+    poolInfo = get_pool_Info()
+    print("")
+    votes = get_votes(50,"0xB92524021c43f663F78dbD56Ed5007E110F94998")
+    print("查询加速之后个人池子权重", votes)
+    weights = get_weights("0xB92524021c43f663F78dbD56Ed5007E110F94998")
+    print("查询加速之后个人池子总权重", weights)
+    tokenPerBlock = get_tokenPerBlock()
+    print("每个块产生的奖励数量",tokenPerBlock / 10 ** 18)
+    totalAllocPoint = get_totalAllocPoint()
+    print("所有池子总权重",totalAllocPoint)
 
--- contract/gauge.py
+++ contract/gauge.py
@@ -1,21 +1,32 @@
 from app import *
 
-ret = init()
-gauge = ret[4]
-print("池子的gauge地址", gauge.address)
+def get_gauge():
+    ret = init()
+    gauge = ret[4]
+    return gauge
+gauge = get_gauge()
 
-# total_supply 所有人质押到该池子的lp的数量
-totalSupply = gauge.functions.totalSupply().call()
-print("所有人质押到该池子的lp的数量:", totalSupply / 10 ** 18)
+def get_totalSupply():
+    # total_supply 所有人质押到该池子的lp的数量
+    totalSupply = gauge.functions.totalSupply().call()
+    return totalSupply
 
-# pending：个人质押到池子里的预计的奖励 参数：address 输入一个钱包账户
-pending = gauge.functions.pending("0xdf4e614dc3e91b4D8aaB7CA1622A8771d29C7923").call()
-print("个人质押到池子里的预计的奖励:", pending / 10 ** 18)
+def get_pending(address):
+    # pending：个人质押到池子里的预计的奖励 参数：address 输入一个钱包账户
+    pending = gauge.functions.pending(address).call()
+    return pending
 
-# userInfo：个人质押lp到此池子的数量 参数：address 输入一个钱包账户
-userInfo = gauge.functions.userInfo("0xdf4e614dc3e91b4D8aaB7CA1622A8771d29C7923").call()
-print("个人质押lp到此池子的数量", userInfo[0] / 10 ** 18)
+def get_userInfo(address):
+    # userInfo：个人质押lp到此池子的数量 参数：address 输入一个钱包账户
+    userInfo = gauge.functions.userInfo(address).call()
+    return userInfo
 
-
-def get_userinfo(account):
-    return gauge.functions.userInfo(account).call()
+if __name__ == '__main__':
+    gauge = get_gauge()
+    print("池子的gauge地址", gauge.address)
+    totalSupply = get_totalSupply()
+    print("所有人质押到该池子的lp的数量:", totalSupply / 10 ** 18)
+    pending = get_pending("0xdf4e614dc3e91b4D8aaB7CA1622A8771d29C7923")
+    print("个人质押到池子里的预计的奖励:", pending / 10 ** 18)
+    userInfo = get_userInfo("0xdf4e614dc3e91b4D8aaB7CA1622A8771d29C7923")
+    print("个人质押lp到此池子的数量", userInfo[0] / 10 ** 18)
